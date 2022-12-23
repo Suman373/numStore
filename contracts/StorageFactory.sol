150f886c2ae52c824f54d38dc89b11fc02872e75
Suman Roy <95040233+Suman373@users.noreply.github.com>
2022-12-23 17:32:07 +0530

    add: sFstore() & sfGet()
    create and read numbers on arr in a s.c

--- contracts/StorageFactory.sol
+++ contracts/StorageFactory.sol
@@ -12,9 +12,17 @@ contract StorageFactory{
         SimpleStorage simpleStorage = new SimpleStorage(); // initialised at 0x0000... blank
         simpleStorageArr.push(simpleStorage); // create the new contract and store in arr to keep a track
     }
-
+    // storage factory -> create
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNum) public{
         // interaction with s.c requires 2 things ->>>
         // 1. Address of the deployed s.c   2. Application Binary Interface (ABI)
+        // creating obj simpleStorage, accessing the address for given index
+        SimpleStorage simpleStorage = simpleStorageArr[_simpleStorageIndex];
+        // storing the given num inside the simpleStorage obj
+        simpleStorage.store(_simpleStorageNum);
     } 
+    // storage factory -> read
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+         return simpleStorageArr[_simpleStorageIndex].retrieve(); // calls the retrieve function on the array item -> s.c
+    }
 }
